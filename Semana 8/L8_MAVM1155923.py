e5af9a074f0d5b679263f9f936e3375a89903ab9
Mariiiiio <143531256+Mariiiiio@users.noreply.github.com>
2023-09-26 10:06:58 -0600

    Add files via upload

---
+++ Semana 8/L8_MAVM1155923.py
@@ -0,0 +1,38 @@
+
+while True:
+ print("Menu:")
+ print("1. factorial")
+ print("2. Secuencia de Fibonacci")
+ print("3. salir")
+ r=input()
+ r=int(r)
+
+ if r == 1:
+  print("ingrese un numero")
+  n1=input()
+  n1=int(n1)
+  factorial=1
+  factorial=int(factorial)
+
+  for i in range(1, n1+1):
+    factorial=factorial*i
+  print(str(n1)+"*...*2*1= "+str(factorial))
+
+ elif r == 2:
+  x=0
+  y=1
+  z=0
+  print("ingrese un numero")
+  n2=input(":")
+  n2=int(n2)
+
+ print(str(x)+", "+str(y)+", ",end=" ") 
+ for s in range(0,n2):
+   z=x+y
+   print(f"{z},",end=" ")
+   x=y
+   y=z
+ print("Fibonacci: ("+str(n2)+")")
+
+ if r == 3:
+  break
