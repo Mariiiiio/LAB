ef141a4d1a7b9f9faaca0d3829ad332ff6a8deb0
Mariiiiio <143531256+Mariiiiio@users.noreply.github.com>
2023-10-10 10:39:11 -0600

    Add files via upload

---
+++ Semana 10/L10_MAVM1155923.py
@@ -0,0 +1,31 @@
+
+
+def obtener_hexadecimal(valor):
+    valor = str(valor)
+    n_l = {
+        "10": "A",
+        "11": "B",
+        "12": "C",
+        "13": "D",
+        "14": "E",
+        "15": "F",
+    }
+    if valor in n_l:
+        return n_l[valor]
+    else:
+        return valor
+
+
+def decimal_hexadecimal(decimal):
+    hexadecimal = ""
+    while decimal > 0:
+        residuo = decimal % 16
+        verdadero_caracter = obtener_hexadecimal(residuo)
+        hexadecimal = verdadero_caracter + hexadecimal
+        decimal = int(decimal / 16)
+    return hexadecimal
+
+decimal = int(
+    input("Escribe un número: "))
+hexadecimal = decimal_hexadecimal(decimal)
+print("El decimal " + str(decimal)+ " es " + str(hexadecimal) + " en hexadecimal")
